6b0f8b9426606715252b5d39619c97c850bbae47
Vlad <Vlad>
2017-04-03 00:10:19 +0300

    some work with server(plz don't check)

--- .UI/app.js
+++ .UI/app.js
@@ -1,18 +1,65 @@
 var express = require('express');
-
+var bodyParser = require('body-parser');
 var app = express();
-
+var db = require('diskdb');
+db.connect('private', ['tags', 'articles','deletedArticles']);
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/hello', function (req, res) {
+app.get('/news', function (req, res) {
+    res.json(db.articles.find());
+    console.log("News send");
+});
 
-    res.send('Hello World!');
+app.get('/tags', function (req, res) {
+    res.json(db.tags.find());
+    console.log("Tags send");
+});
 
+app.post('/news', function (req, res) {
+    console.log("POST");
+    db.articles.remove();
+    db.loadCollections(['articles']);
+    db.articles.save(req.body);
 
 });
 
+app.post('/tags', function (req, res) {
+    console.log("POST");
+    db.tags.remove();
+    db.loadCollections(['tags']);
+    db.tags.save(req.body);
+});
+
+app.path('/news/', function (req, res) {
+    let index = req.body.id;
+    let query = db.articles.findOne({id: index.toString()});
+    console.log(query);
+    console.log(req.body);
+    var options = {
+        multi: false,
+        upsert: false
+    };
+    db.articles.update(query, req.body, options);
+    res.json(req.body);
+});
+app.get('/news/:id', function (req, res) {
+    let article = db.articlse.findOne({id: req.params.id});
+    res.json(article);
+});
+app.delete('/news/:id', function (req, res) {
+    let id = req.params.id;
+    let article = db.articles.findOne({id: req.params.id});
+    db.deletedArticles.save(article);
+    db.articles.remove({id: id});
+    res.json({idWasRemoved: Number(id)});
+});
+app.put('/news/', function (req, res) {
+    db.articles.save(req.body);
+    res.json(req.body);
+});
 app.listen(3000, function () {
 
-    console.log('Example app listening on port 3000!');
-
+    console.log('Example app listening on port 3000! NEW!!');
+    console.log(db.articles.find());
 });
---
+++ .UI/public/scripts/serverWorker.js
@@ -0,0 +1,65 @@
+/**
+ * Created by Мдфв on 02.04.2017.
+ */
+var serverWorker = (function () {
+    function globalGet() {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", '/news', false);
+        xhr.send();
+        var articles = JSON.parse(xhr.responseText);
+        articles.forEach(function (article) {
+            article.createdAt = new Date(article.createdAt);
+        });
+        xhr.open("GET", '/tags', false);
+        xhr.send();
+        var tags = JSON.parse(xhr.responseText);{
+            return {
+                articles:articles,
+                tags:tags
+            };
+        }
+    }
+
+    function globalPost(articles) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(articles['articles']));
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/tags');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(articles['tags']));
+    }
+
+    function updateArticle(article) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("PATH", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    function getFullArticle(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", '/news/'+id);
+        xhr.send();
+        var article = JSON.parse(xhr.responseText);
+        return article;
+    }
+    function deleteArticle(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('DELETE','/news/'+id);
+        xhr.send();
+    }
+    function sendArticle(article) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", '/news');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(article));
+    }
+    return{
+        globalGet:globalGet,
+        globalPost:globalPost,
+        updateArticle:updateArticle,
+        getFullArticle:getFullArticle,
+        deleteArticle:deleteArticle
+    }
+}());
